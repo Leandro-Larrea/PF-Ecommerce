1ec16951b012b2894acde527191323299d172cd6
unknown <meliodas_17@hotmail.com>
2022-11-22 19:14:51 -0300

    usando stash

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 const productMidleware = require("./productsRouter")
+const userMidleware = require("./usersRouter")
 
 router.use("/products", productMidleware)
+router.use("/users", userMidleware)
 
 module.exports = router;
