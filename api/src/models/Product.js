af5aae29c703b09cb3885986456bbb708797baad
yeiberey <yeiberey@gmail.com>
2022-11-24 14:25:55 -0500

    model product

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,79 +1,91 @@
-const {Schema, model, default: mongoose} = require("mongoose");
+const { Schema, model, default: mongoose } = require("mongoose");
 
 /* trim is gonna clean up the spaces at the end and begining of the string "   asd  ".trim() = "asd"*/
 
 const reviewsSchema = new Schema({
-    user:{
-        type: String,
-        required: true,
-    },
-    review:{
-        type: String,
-        required: true,
-        trim: true
-    }
+  user: {
+    type: String,
+    required: true,
+  },
+  review: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 });
 
 const ratingSchema = new Schema({
-    points:{
-        type: Number
+  points: {
+    type: Number,
+  },
+  votes: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    default: function () {
+      if (this.points > 0 || this.votes > 0) return this.points / this.votes;
+      return 0;
     },
-    votes:{
-        type: Number,
-        required: true
-    },
-    rating: {
-        type: Number,
-        default: function() {
-            if(this.points > 0 || this.votes > 0) return this.points / this.votes ;
-              return 0
-        }
-    }
+  },
+});
+const imageSchema = new Schema({
+  image: {
+    type: String,
+    required: true,
+  },
+  imageId: {
+    type: String,
+    required: true,
+  },
 });
 
-const productSchema = new Schema({
-    title:{
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    price:{
-        type: Number,
-        required: true,
-        trim: true 
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
     },
-    description:{
-        type: String,
-        required: true,
-        trim: true
+    description: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    category:{
-        type: String,
-        required: true,
-        trim:true
+    category: {
+      type: String,
+      required: true,
+      trim: true,
     },
     stock: {
-        type: Number
+      type: Number,
     },
     rating: {
-        type: ratingSchema,
-        required: true
+      type: ratingSchema,
+      required: true,
     },
-    image:{
-        type: String,
-        required: true
+    image: {
+      type: String,
+      required: true,
     },
-    details:{
-        type: Array
+    imageCloudinary: imageSchema,
+    details: {
+      type: Array,
     },
-    reviews:[reviewsSchema]
-},
-    {
-        timestamps: true,
-        versionKey: false
-    },
+    reviews: [reviewsSchema],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
- const Product = model("Product", productSchema);
- module.exports = Product;
+const Product = model("Product", productSchema);
+module.exports = Product;
