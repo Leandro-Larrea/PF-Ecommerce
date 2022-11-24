a607fc8d6a94ce7b55c750425fbc6d64a4b158cc
unknown <meliodas_17@hotmail.com>
2022-11-24 17:54:55 -0300

    get by query already working =D

--- api/src/routes/productsRouter.js
+++ api/src/routes/productsRouter.js
@@ -1,48 +1,45 @@
 const { Router } = require("express");
 const router = Router();
-const getApiProducts = require("../controllers/getProducts.js");
+const { getProducts } = require("../controllers/getProducts.js");
 const Product = require("../models/Product.js");
 
 router.get("/", async(req, res) =>{
     try {
+        let {name} = req.query
+        if(name){
+           let filterProducts = await getProducts(name)
+           return res.status(200).json(filterProducts)
+        }
         let products = await Product.find();
+        products = products.map((e)=> {return {title: e.title, image: e.image}})
          res.status(200).json(products);
     } catch (error) {
-         res.status(404).send("le erraste chamigo");
+         res.status(404).send("it doesn't work");
     }
 });
-
-
 
 router.post("/", async (req, res)=>{
     try {
         let obj = req.body
-        if(Array.isArray(obj)){
-            for(const e of obj){
-            let product = {
-                title:e.title,
-                price:e.price,
-                description:e.description ,
-                category: e.category,
-                image: e.image,
-                rating:{
-                points: 0,
-                votes: 0
-                },
-                details:e.details
-        }
-        let a = Product(product)
-        let s = await a.save()
-            }
-            let productsDb = await Product.find();
-            return res.status(200).json(productsDb);
-        }
+       
    const a = Product(obj)
    const s = await a.save()
    return res.status(200).json(s)
     } catch (error) {
-        res.status(400).send("something get wrong posting")
+        res.status(400).json(error)
     } 
 });
+
+router.put("/:id", async (req,res)=>{
+    try {
+    let {id} = req.params
+
+    let a = await Product.findByIdAndUpdate(id, req.body);
+    let b = await Product.findById(id);
+        res.status(200).json({a,b})
+    } catch (error) {
+        res.status(400).send("something get wrong")
+    } 
+})
 
 module.exports = router;
