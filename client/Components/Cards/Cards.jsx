b2ad6a974e2feb5091d942b23ad8e31777b115fd
MalenaLuana <malenaluana98@gmail.com>
2022-11-24 16:18:08 -0300

    Componente Cards

---
+++ client/Components/Cards/Cards.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+
+import { View, Text, StyleSheet } from 'react-native'
+import {useDispatch,useSelector} from 'react-redux'
+import * as actions from '../../redux/actions'
+
+import Card from "./Card";
+
+
+
+export default function Cards(){
+
+ const dispatch = useDispatch()
+
+ const data = useSelector(state=>state.products)
+ const products= data.products
+
+ 
+useEffect(()=>{
+    dispatch(actions.getProducts())
+
+},[])
+
+    return(
+    <View>
+        <Text>Cards</Text>
+
+        {
+            products.length ?
+            products.map(e=>{
+            return(
+             <Card title={e.title} image={e.image} price={e.price} key={e.title}></Card>)
+            })
+            :<Text>no se esta renderizando</Text>
+        }
+    </View>
+    )
+}
--- client/Components/LookProducts/LookProducts.jsx
+++ client/Components/LookProducts/LookProducts.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React ,{useEffect,useState}from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+
+
+//--componentes---
+import Cards from '../Cards/Cards.jsx'
+
 export const LookProducts = () => {
+
+
+
+
+
   return (
-    <View>
-        <Text style={style.text} >Productos aca</Text>
+    <View>  
+     <View><Text>search</Text></View>
+     <View><Text>filters</Text></View>
+     <Cards></Cards>
+       
     </View>
   )
 }
@@ -12,5 +25,10 @@ export const LookProducts = () => {
 const style = StyleSheet.create({
     text: {
         backgroundColor: "red"
+    },
+    play:{
+    
+      height:'100%'
     }
+
 })
