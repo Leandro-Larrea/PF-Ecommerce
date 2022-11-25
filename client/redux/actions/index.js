d72fbcc198403690d8820dd120f2ada1c97eb87a
MalenaLuana <malenaluana98@gmail.com>
2022-11-25 00:58:03 -0300

    actions actualizadas

--- client/redux/actions/index.js
+++ client/redux/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 
 export const GET_PRODUCTS = 'GET_PRODUCTS'
+export const GET_CATEGORIES='GET_CATEGORIES'
 export const SEARCH = 'SEARCH';
 
 
@@ -11,7 +12,7 @@ export const getProducts =  () => dispatch =>{
    /*cada uno tiene que poner su propia IP*/ 
     return axios.get("http://192.168.0.94:3001/products")
     .then (res => {
-        console.log(res.data)
+
         dispatch({
             type:"GET_PRODUCTS",
             payload:res.data
@@ -19,6 +20,15 @@ export const getProducts =  () => dispatch =>{
     })
 }
 
+export const getCategories =  ()=> dispatch =>{
+    return axios.get("http://192.168.0.94:3001/categories")
+    .then (res=> {
+        dispatch({
+            type :"GET_CATEGORIES",
+            payload:res.data
+        })
+    })
+}
 export const search = (name) => {
     return async function (dispatch) {
         return await axios(`http://192.168.0.94:3001/products?name=${name}`)
