e2f60ab79bc9ae0051852854817ec87052492c5d
unknown <meliodas_17@hotmail.com>
2022-11-25 13:28:58 -0300

    backup working

--- api/src/cloudinary/uploadToCloudinary.js
+++ api/src/cloudinary/uploadToCloudinary.js
@@ -2,19 +2,19 @@ require('dotenv').config()
 var fs = require('fs')
 const {cloudinary} = require('./utils')
 
-const image = './messi.jpg'
-const uploadToCloudinary = async (file = image) => {
-    let buffer = fs.readFileSync(file)
-    await fs.writeFile('guardado.jpg', buffer, (error) => {
-        if (error) console.log('error: ', error)
-    }) 
+const uploadToCloudinary = async (file) => {
+    // let buffer = fs.readFileSync(file)
+    // await fs.writeFile('guardado.jpg', buffer, (error) => {
+    //     if (error) console.log('error: ', error)
+    // }) 
       
-    let result = await cloudinary.v2.uploader.upload('./guardado.jpg')
-    fs.unlinkSync('./guardado.jpg')
+    // let result = await cloudinary.v2.uploader.upload('./guardado.jpg')
+    // fs.unlinkSync('./guardado.jpg')
+    let result = await cloudinary.v2.uploader.upload(file)
 
     return {
-        'public_id': result.public_id,
-        'url': result.url
+        'imageId': result.public_id,
+        'image': result.url
         }
     
 }
