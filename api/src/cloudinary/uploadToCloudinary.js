1e8c672e130bf1d02941e3393293aa30b7efab81
unknown <meliodas_17@hotmail.com>
2022-11-26 01:08:29 -0300

    cloudinary reparado

--- api/src/cloudinary/uploadToCloudinary.js
+++ api/src/cloudinary/uploadToCloudinary.js
@@ -2,21 +2,19 @@ require('dotenv').config()
 var fs = require('fs')
 const {cloudinary} = require('./utils')
 
-const uploadToCloudinary = async (file = image) => {
+const uploadToCloudinary = async (file) => {
+    console.log("cloudinary")
     //let buffer = fs.readFileSync(file)
     
-    await fs.writeFile('guardado.jpg', buffer, (error) => {
-        if (error) console.log('error: ', error)
-    }) 
+    // await fs.writeFile('guardado.jpg', buffer, (error) => {
+    //     if (error) console.log('error: ', error)
+    // }) 
+    // fs.unlinkSync('./guardado.jpg')
       
-    let result = await cloudinary.v2.uploader.upload('./guardado.jpg')
-    
-    fs.unlinkSync('./guardado.jpg')
-
+    let result = await cloudinary.v2.uploader.upload(file)
     return {
-        'title': title,
-        'public_id': result.public_id,
-        'url': result.url
+        'imageId': result.public_id,
+        'image': result.url
     }
     
 }
