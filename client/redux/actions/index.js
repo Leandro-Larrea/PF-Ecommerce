e9191d07b2b954167b69744becdba42f1a600caa
yeiberey <yeiberey@gmail.com>
2022-11-28 00:14:10 -0500

    pushYeiber

--- client/redux/actions/index.js
+++ client/redux/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const GET_PRODUCTS = 'GET_PRODUCTS';
 export const SEARCH = 'SEARCH';
 export const GET_CATEGORIES = 'GET_CATEGORIES';
-export const FILTER_CATEGORIES = 'FILTER_CATEGORIES'
+export const FILTER_CATEGORIES = 'FILTER_CATEGORIES';
 
 export const getProducts = () => dispatch => {
   /*cada uno tiene que poner su propia IP*/
@@ -16,30 +16,28 @@ export const getProducts = () => dispatch => {
   });
 };
 
-export const getCategories =  ()=> dispatch =>{  
- 
-  return axios.get("/categories")
+export const getCategories = () => dispatch => {
+  return axios
+    .get('/categories')
 
-  .then (res=> {
-    
+    .then(res => {
       dispatch({
-          type :GET_CATEGORIES,
-          payload:res.data
-      })
-  })
-}
+        type: GET_CATEGORIES,
+        payload: res.data,
+      });
+    });
+};
 
-export const filterByCategories = (category)=>dispatch=>{
-    return axios.get(`/products/${category}`)
-    .then(res=>{
-      dispatch({
-        type:'FILTER_CATEGORIES',
-        payload:res.data
-      })
-    })
-}
+export const filterByCategories = category => dispatch => {
+  return axios.get(`/products/${category}`).then(res => {
+    dispatch({
+      type: 'FILTER_CATEGORIES',
+      payload: res.data,
+    });
+  });
+};
 
-export const search = (name) => {
+export const search = name => {
   return async function (dispatch) {
     return await axios(`/products?name=${name}`).then(res =>
       dispatch({
@@ -49,5 +47,3 @@ export const search = (name) => {
     );
   };
 };
-
-
