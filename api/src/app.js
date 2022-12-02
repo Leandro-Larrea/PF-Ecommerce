b53592fa0ff4e1acdc244277d75a84f2cd5cc390
unknown <meliodas_17@hotmail.com>
2022-12-02 00:07:12 -0300

    back stripe

--- api/src/app.js
+++ api/src/app.js
@@ -3,15 +3,18 @@ const routes = require('./routes/index.js');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 
 const server = express()
 
 server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use("/payments/stripe", express.raw({ type: "*/*" }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use(cors());
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
