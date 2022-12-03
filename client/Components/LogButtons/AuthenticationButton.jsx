83f23fbea0a62cce26fb422d0bdfecd9763ccdc7
EseAH <ese.aquino@gmail.com>
2022-12-03 14:01:28 -0300

    merge cambios mati

--- client/Components/LogButtons/AuthenticationButton.jsx
+++ client/Components/LogButtons/AuthenticationButton.jsx
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import { View } from 'react-native';
-// import {useAuth0} from 'react-native-auth0';
-// import { LoginButton} from './LoginButton'
-// import { LogoutButton } from './LogoutButton';
- 
-
-// const AuthenticationButton = () => {
-//   const {user} = useAuth0()
-  
-//   return (
-//     <>
-//         {user ? (
-//             <View>
-//                 <LogoutButton />
-//             </View>
-//         )
-//         : (
-//             <View>
-//                 <LoginButton />
-//             </View>
-//         )}
-//     </>
-//     )
-// }
-
 import React from 'react';
 import {Button, Text, View, StyleSheet} from 'react-native';
 import {useAuth0} from 'react-native-auth0';
