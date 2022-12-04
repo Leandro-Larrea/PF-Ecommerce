338feb6dbea52c610fe4feaf58be3122dc184f67
EseAH <ese.aquino@gmail.com>
2022-12-04 19:49:05 -0300

    auth en boton en detail

--- client/Components/Products/DetailProduct.jsx
+++ client/Components/Products/DetailProduct.jsx
@@ -10,11 +10,27 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import {stylesCardProduct} from '../../styles';
 import {CartContext} from '../Cart/ShoppingCart';
+import {useAuth0} from 'react-native-auth0'
 
 function DetailProduct({route, navigation}) {
   const {_id, title, image, description, price} = route.params;
   const {cartItems, addItemToCart, deleteItemToCart} = useContext(CartContext);
   const inCart = cartItems.find(product => product.productId === _id);
+  //---Auht0
+  const {user} = useAuth0();
+  const {authorize} = useAuth0();
+  const handleLoginD = async () => {
+    return await authorize()
+  }
+
+  const handleButtonAddD = async (inCart) => {
+    if(inCart) {
+      await deleteItemToCart(route.params)
+      navigation.goBack();
+    }
+    else await addItemToCart(route.params, 1)
+  }
+
   return (
     <View style={styles.container} title={title}>
       <Image
@@ -41,12 +57,10 @@ function DetailProduct({route, navigation}) {
           color={inCart ? '#FF4544' : '#65AE77'}
           style={{margin: 10}}
           onPress={() => {
-            if (inCart) {
-              deleteItemToCart(route.params);
-              navigation.goBack();
-              return;
-            }
-            addItemToCart(route.params, 1);
+            user ?
+            handleButtonAddD(inCart)
+            :
+            handleLoginD()
           }}>
           <Icon size={20} name="cart-plus" color="#fff" />
         </Button>
