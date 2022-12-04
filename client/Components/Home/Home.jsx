a517c7dea48bdb9874f8159993d94885e7f9dfd8
MalenaLuana <malenaluana98@gmail.com>
2022-12-04 19:48:01 -0300

    home style + carousel

--- client/Components/Home/Home.jsx
+++ client/Components/Home/Home.jsx
@@ -5,8 +5,13 @@ import {useDispatch, useSelector} from 'react-redux';
 import {getCategories, getProducts} from '../../redux/actions';
 import AuthenticationButton from '../LogButtons/AuthenticationButton'
 import Icon from 'react-native-vector-icons/Ionicons';
+
+
 import HomeCategories from './HomeCategories';
 import Header from './Header';
+import Carousel from './Carousel';
+
+
 
 export const Home = ({navigation}) => {
   const dispatch = useDispatch();
@@ -24,15 +29,17 @@ export const Home = ({navigation}) => {
         <Header></Header>
       </View>
       <View style={style.cuenta}>
-        <AuthenticationButton/>
-        <TouchableHighlight onPress={()=> navigation.navigate('Profile')}>
-            <Icon name="person-outline" size={28} color="#6A37C4" />
+        
+        <TouchableHighlight style={style.icon} onPress={()=> navigation.navigate('Profile')}>
+            <Icon name="person-outline" size={28} color="#89c30d" />
         </TouchableHighlight>
+        <AuthenticationButton/>
+
       </View>
+
       <View style={style.content}>
-        <View style={style.ofertaBox}>
-          <Text>OFERTAS 🔥</Text>
-        </View>
+
+        <Carousel></Carousel>
         <View style={style.categoriaBox}>
           <Text style={style.categoriesText}>Suggested categories</Text>
           {categories ? (
@@ -43,6 +50,7 @@ export const Home = ({navigation}) => {
             <Text>cargando</Text>
           )}
         </View>
+
       </View>
       {/* <NavBar/> */}
     </SafeAreaView>
@@ -55,7 +63,6 @@ const style = StyleSheet.create({
   },
   content: {
     flex: 10,
-    padding: 5,
   },
   header: {
     flex: 1,
@@ -66,13 +73,7 @@ const style = StyleSheet.create({
   headerText: {
     textAlign: 'center',
   },
-  ofertaBox: {
-    flex: 1,
-    borderWidth: 2,
-    borderColor: 'black',
-    margin: 30,
-    backgroundColor: '#6A37C4',
-  },
+
   categoriaBox: {
     marginTop: -10,
     marginLeft: 20,
@@ -85,10 +86,17 @@ const style = StyleSheet.create({
     textAlign: 'center',
     flex: 0,
     marginTop: 50,
+    color:'black'
   },
   cuenta: {
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    justifyContent:'center',
     alignItems: 'center',
+    minHeight:80,
+    backgroundColor:'black'
+    
+  },
+  icon:{
+    marginRight:10
   }
 });
