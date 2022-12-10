f1d6490a65d1af5a0a210c75fd944944a9d1e3bf
EseAH <ese.aquino@gmail.com>
2022-12-10 03:42:11 -0300

    cambios

--- client/Components/User/Purcheases.jsx
+++ client/Components/User/Purcheases.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { stylesCardProduct } from '../../styles'
 
 export const Purcheases = () => {
   return (
     <View>
-        <Text>
+        <Text style={stylesCardProduct.title}>
             Purcheases
         </Text>
         <Text>
