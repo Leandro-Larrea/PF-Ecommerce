9524e2fdea5c8faa55cb21a529e6697bc6793870
MatiasSoto21 <matiassoto00@outlook.com>
2022-12-10 20:41:35 -0300

    carrito validado y pay mejorado falta actualizar el user

--- client/Components/Stripe/Pay.jsx
+++ client/Components/Stripe/Pay.jsx
@@ -1,16 +1,41 @@
 import { useStripe } from '@stripe/stripe-react-native'
-import React, { useState } from 'react'
-import { View, TextInput, Button, Alert, Text, StyleSheet } from 'react-native'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { View, TextInput, Button, Alert, Text, StyleSheet, ScrollView } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 import { LocalNotification, ScheduleNotification } from '../../src/services/LocalPushControllers'
+import { useAuth0 } from 'react-native-auth0';
+import { getUser } from '../../redux/actions'
 
-export const Pay = ({navigation}) => {
-  const total = useSelector(state => state.total);
-  const [name, setName] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [mail, setMail] = useState('');
-  const [price, setPrice] = useState(total);
+
+export const Pay = ({ navigation }) => {
+
+  const { user } = useAuth0();
+  const dispatch = useDispatch();
+
+  const userDb = useSelector(state => state.user)
+
+  useEffect(() => {
+    if (user) {
+      dispatch(getUser(user.sub))
+    }
+  }, [])
+
+  useEffect(() => {
+    if (userDb) {
+      setInput(userDb);
+    }
+  }, []);
+
+  const [input, setInput] = useState({
+    name: '',
+    lastName: '',
+    location: { country: "", city: "", address: "" },
+    image: {},
+    phone: "",
+    mail: user?.email
+  })
+  const total = useSelector(state => state.total).toFixed(2);
 
   const navegar = () => {
     navigation.navigate('Products')
@@ -18,17 +43,17 @@ export const Pay = ({navigation}) => {
 
   const stripe = useStripe();
   /* 192.168.100.32 */
+  /*   'https://pf-ecommerce-rho.vercel.app/payments/pay' */
   const subscribe = async () => {
     try {
       const response = await fetch('https://pf-ecommerce-rho.vercel.app/payments/pay', {
         method: 'POST',
-        body: JSON.stringify({ name, lastname, mail, price }),
+        body: JSON.stringify({ input, total }),
         headers: {
           "Content-Type": "application/json"
         }
       });
       const data = await response.json();
-      console.log("response", response);
       if (!response.ok) return Alert.alert(data.message);
       const clientSecret = data.clientSecret;
       const initSheet = await stripe.initPaymentSheet({
@@ -42,49 +67,125 @@ export const Pay = ({navigation}) => {
       if (presentSheet.error) return Alert.alert(presentSheet.error.message)
       Alert.alert('Payment Complete, thank you!');
       navegar();
-      axios.post(`https://pf-ecommerce-production-ed4d.up.railway.app/payments/email`, {mail, name, lastname, price})
-      .then(console.log("exitoso"))
+     /*  https://pf-ecommerce-production-ed4d.up.railway.app/payments/email */
+      axios.post(`https://pf-ecommerce-production-ed4d.up.railway.app/payments/email`, { input, total})
       LocalNotification();
       ScheduleNotification();
-
     } catch (err) {
-      console.log("data");
       console.error(err);
       Alert.alert("Fail, try again")
     }
   }
 
+
+
   return (
-    <View>
-      <View style={style.viewInput}>
+    <ScrollView>
+      <View style={{
+        marginTop: 15,
+        marginBottom: 15,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <Text style={{
+          fontSize: 25,
+          color: "#cdcdcd",
+          backgroundColor: "#2d2d2d",
+          padding: 12,
+          paddingHorizontal: 80,
+          borderRadius: 20,
 
-        <Text>Name</Text>
-        <TextInput value={name} onChangeText={text => setName(text)} placeholder="Name..."
-          style={style.textInput}
-          />
+        }}>Billing Details</Text>
       </View>
-      <View style={style.viewInput}>
 
-        <Text>LastName</Text>
 
-        <TextInput value={lastname} onChangeText={text => setLastname(text)} placeholder="LastName..."
-          style={style.textInput}
-          />
-      </View>
-      <View style={style.viewInput}>
-
+      <View style={style.viewDetail}>
         <Text>Mail</Text>
-
-        <TextInput value={mail} onChangeText={text => setMail(text)} placeholder="example@example.com"
-          style={style.textInput}
-        />
+        <Text
+          style={style.textDetail}>
+          {input.mail}
+        </Text>
       </View>
+
+
+
+      <View style={style.viewDetail}>
+        <Text>Name</Text>
+        <Text
+          style={style.textDetail}>
+          {input.name}
+        </Text>
+      </View>
+
+
+
+      <View style={style.viewDetail}>
+        <Text>LastName</Text>
+        <Text
+          style={style.textDetail}>
+          {input.lastName}
+        </Text>
+      </View>
+
+      <View style={style.viewDetail}>
+        <Text>Phone</Text>
+        <Text
+          style={style.textDetail}>
+          {input.phone}
+        </Text>
+      </View>
+
+      <View style={style.viewDetail}>
+        <Text>Country</Text>
+        <Text
+          style={style.textDetail}>
+          {input.location.country}
+        </Text>
+      </View>
+
+      <View style={style.viewDetail}>
+        <Text>City</Text>
+        <Text
+          style={style.textDetail}>
+          {input.location.city}
+        </Text>
+      </View>
+
+      <View style={style.viewDetail}>
+        <Text>Address</Text>
+        <Text
+          style={style.textDetail}>
+          {input.location.address}
+        </Text>
+      </View>
+
+      <View style={{
+        marginTop: 15,
+        marginBottom: 15,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <Text style={{
+          fontSize: 20,
+          width: "100%",
+          color: "green",
+          backgroundColor: "#2d2d2d",
+          padding: 12,
+          paddingRight: 80,
+
+        }}>Order summary:      ${total}  USD</Text>
+      </View>
+
       <View style={{ marginTop: 20 }}>
         <Button title="Pay Now" onPress={subscribe} ></Button>
       </View>
-    </View>
+
+    </ScrollView>
   )
 }
+
+
+
 
 const style = StyleSheet.create({
   viewInput: {
@@ -92,10 +193,17 @@ const style = StyleSheet.create({
     marginTop: 10,
     marginBottom: 5
   },
-  textInput: {
-    width: 300,
-    fontSize: 18,
-    padding: 10,
-    borderWidth: 1,
+  viewDetail: {
+    padding: 12
   },
+  textDetail: {
+    color: "black",
+    opacity: 0.8,
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderColor: "#CDCDCD",
+    paddingVertical: 10,
+  },
+
+
 })
