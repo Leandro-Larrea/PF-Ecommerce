8f22245dab423d1fcd7f4378be2196496899610e
unknown <meliodas_17@hotmail.com>
2022-12-12 01:13:21 -0300

    post product mas bonito

--- admin/src/components/postProduct/PostProduct.jsx
+++ admin/src/components/postProduct/PostProduct.jsx
@@ -15,9 +15,9 @@ export const PostProduct = () => {
   const categories = useSelector(state => state.categories)
   const [input, setInput] = useState({
       title: '',
-      price: '',
+      price: '$',
       description: '',
-      category: '',
+      category: null,
       image: '',
       stock: '',
       rating: {points:0 , votes: 0}
@@ -57,10 +57,14 @@ export const PostProduct = () => {
   }
 
   function handleChange(e){
-    if(e.target.name === 'stock')
-      setInput({...input, [e.target.name]: parseInt(e.target.value)})
-    else
-      setInput({...input, [e.target.name]: e.target.value})
+    let a = e.target.value
+    if(e.target.name === 'stock'){
+      setInput({...input, [e.target.name]: parseInt(a)})
+      return}   
+      if(e.target.name === "price" &&  /\d/.test(a) || a === "$"){ 
+    setInput({...input, [e.target.name]: a})}
+    else if(e.target.name !== "price")
+      setInput({...input, [e.target.name]: a})
   }
 
 
@@ -70,17 +74,19 @@ export const PostProduct = () => {
       alert('Completar todos los campos')
     } 
     else { 
-      console.log('lleno el input', input)
-      let res = await dispatch(postProduct(input));
+      let a = {...input, price: parseInt(input.price.slice(1))}
+      console.log('lleno el input', a)
+      let res = await dispatch(postProduct(a));
       console.log('listorti: ', res)
       alert('Producto Creado 👍 ')
       setInput({
         title: '',
-        price: '',
+        price: '$',
         description: '',
-        category: '',
+        category: "",
         image: '',
-        stock: ''
+        stock: '',
+        rating: {points:0 , votes: 0}
     })
     }
   }
@@ -94,15 +100,15 @@ export const PostProduct = () => {
             <form onSubmit={handleSubmit} className="form">
               <div className='itemContainer'>
                 <label classname="label">Product: </label>
-                <input classname="inputI" type='text' name='title' onChange={handleChange} value={input.title}/>
+                <input classname="inputI" type='text' placeholder='Playstaion' name='title' onChange={handleChange} value={input.title}/>
               </div>
               <div className='itemContainer'>
                 <label classname="label">Price:</label>
-                <input classname="inputI" type='number' name='price' onChange={handleChange} value={input.price} />
+                <input classname="inputI" type='text' placeholder="00" name='price' onChange={handleChange} value={input.price} />
               </div>
               <div className='itemContainer'>
                 <label classname="label">Stock:</label>
-                <input classname="inputI" type='number' name='stock' onChange={handleChange} value={input.stock}/>
+                <input classname="inputI" type='number' placeholder="00" name='stock' onChange={handleChange} value={input.stock}/>
               </div>
               <div className='itemContainer'>
                 <label classname="label">Category: </label>
@@ -114,22 +120,23 @@ export const PostProduct = () => {
                             )): null}
                 </select>
               </div>
+              <div className='itemContainerText'>
+                <label classname="label">Description:</label>
+                <textarea classname="inputI" name='description' onChange={handleChange} value={input.description}/>
+              </div>
               <div className='itemContainer'>
-                <label classname="label" for="image">Image:</label>
+                <label classname="label" htmlFor="image">Image:</label>
                 <input classname="inputI" type='file' name='image' onChange={handleFileInputChange} />
               </div>
               <div className='itemContainer'>
                 <label>Image URL</label>
-                <input type='text' name='image' onChange={handleChange} />
+                <input type='text'placeholder='https://...' name='image' onChange={handleChange} />
               </div>
-              <div className='itemContainerText'>
-                <label classname="label">Description:</label>
-                <input classname="inputI" type='textArea' name='description' onChange={handleChange} value={input.description}/>
-              </div>
-                <input className='button' type='submit' value='Send' />
               <div className='image'>
                 {input.image && <img src={input.image} alt = '' />}
               </div> 
+                <input className='button' type='submit' value='Send' />
+              
             </form>
           </div>
         </div>
