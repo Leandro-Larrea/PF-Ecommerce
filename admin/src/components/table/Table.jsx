23c32a74111e300d791ce730670d743939b51f5f
Leandro Freire <fotografoleandro81@gmail.com>
2022-12-13 04:34:56 -0300

    table

--- admin/src/components/table/Table.jsx
+++ admin/src/components/table/Table.jsx
@@ -1,20 +1,8 @@
 import "./table.scss";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Sidebar from "../sidebar/Sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { deleteProduct, getProduct } from "../../redux/action";
 import { Link } from "react-router-dom";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import RestorePageIcon from '@mui/icons-material/RestorePage';
-
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // icono para usuario
 import { useState } from "react";
 import { PostTable } from "./PostTable";
 
